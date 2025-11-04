2ebbdd4d8084df887618eb099780cc44a1477f29
far1h <farihmuhammad45@gmail.com>
2025-11-04 16:41:56 +0700

    Initial Commit

---
+++ AuthApp/AuthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthAppApp.swift
+//  AuthApp
+//
+//  Created by Farih Muhammad on 04/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct AuthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
